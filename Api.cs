44583fc67a07c671e5d45e779a687907cf2a6773
Andrew Burton <tuglyraisin@gmail.com>
2014-09-20 22:28:17 -0500

    Working on the means to delete keys

--- Api.cs
+++ Api.cs
@@ -16,6 +16,7 @@ namespace SparkCore
         private const string ver = "v1";
         private const string creds = "spark:spark";
 
+        // Use your Spark ID and password to get an access token
         public SparkCore.Json.oauth_token Authenticate(string id, string pw)
         {
             System.Collections.Specialized.NameValueCollection vals = new System.Collections.Specialized.NameValueCollection();
@@ -34,6 +35,7 @@ namespace SparkCore
             return jsonResponse;
         }
 
+        // Call a defined function in the firmware
         public SparkCore.Json.function CallFunction(string did, string f, string at, string args)
         {
             System.Collections.Specialized.NameValueCollection vals = new System.Collections.Specialized.NameValueCollection();
@@ -51,6 +53,33 @@ namespace SparkCore
             return jsonResponse;            
         }
 
+        // Delete an access token
+        public SparkCore.Json.delete_token DeleteToken(string access_token, string id, string pw)
+        {
+            string temp_url = api + "/" + ver + "/access_tokens/" + access_token;
+            string credentials = id + ":" + pw;
+            
+
+            try
+            {
+                WebRequest wr = WebRequest.Create(temp_url);
+                wr.Method = "DELETE";
+                wr.Headers[HttpRequestHeader.Authorization] = "Basic " + Convert.ToBase64String(Encoding.ASCII.GetBytes(credentials));
+                HttpWebResponse res = (HttpWebResponse)wr.GetResponse();
+                if (res.StatusCode != HttpStatusCode.OK) { throw new Exception("HTTP error"); }
+
+                DataContractJsonSerializer jsonSerializer = new DataContractJsonSerializer(typeof(SparkCore.Json.delete_token));
+                object objResponse = jsonSerializer.ReadObject(res.GetResponseStream());
+                SparkCore.Json.delete_token jsonResponse = objResponse as SparkCore.Json.delete_token;
+                return jsonResponse;            
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+
+        // Use this for calls that require POST
         private string PostRequest(string url, System.Collections.Specialized.NameValueCollection data)
         {
             WebClient wc = new WebClient();
@@ -60,7 +89,6 @@ namespace SparkCore
 
             try
             {
-                ServicePointManager.ServerCertificateValidationCallback = delegate { return true; };
                 byte[] retval = wc.UploadValues(url, "POST", data);
                 return Encoding.ASCII.GetString(retval);
             }
@@ -72,6 +100,9 @@ namespace SparkCore
         }
 
         // Constructor
-        public Api() { }
+        public Api()
+        {
+            ServicePointManager.ServerCertificateValidationCallback = delegate { return true; };
+        }
     }
 }
